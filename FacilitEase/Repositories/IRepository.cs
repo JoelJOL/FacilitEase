81f6399b8bf37944453831b42eaff2fa061031c8
Joel <kaduppasseryj28@gmail.com>
2024-01-18 01:30:15 +0530

    added repository and unitofwork

---
+++ FacilitEase/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public interface IRepository<T>
+{
+    IEnumerable<T> GetAll();
+    T GetById(int id);
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
---
+++ FacilitEase/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FacilitEase.UnitOfWork
+{
+    public interface UnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+            Employees = new EmployeeRepository(_context);
+            City = new CityRepository(_context);
+            Designation = new DesignationRepository(_context);
+
+            // Initialize other repositories.
+        }
+
+        public IEmployeeRepository Employees { get; }
+        public ICityRepository City { get; }
+
+        public IDesignationRepository Designation { get; }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+    }
+}
