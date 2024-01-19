4c4c9982f464584505c2b1d56a9221481444f7ac
Joel <kaduppasseryj28@gmail.com>
2024-01-19 19:11:11 +0530

    en:-Added entities

---
+++ FacilitEase/Models/EntityModels/TBL_DEPARTMENT.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FacilitEase.Models.EntityModels
+{
+    public class TBL_DEPARTMENT
+    {
+        [Key]
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
