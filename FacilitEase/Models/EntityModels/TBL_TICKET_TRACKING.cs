ec4b02ce4c39c8fb342e75d3273d6ba737774d5c
Nathaniel Yeldo <nathanielyeldo22@gmail.com>
2024-01-19 18:35:41 +0530

    EN-06 four entity models are added

---
+++ FacilitEase/Models/EntityModels/TBL_TICKET_TRACKING.cs
@@ -0,0 +1,16 @@
+﻿namespace FacilitEase.Models.EntityModels
+{
+    public class TBL_TICKET_TRACKING
+    {
+        public int Id { get; set; }
+        public int TicketId { get; set; }
+        public int TicketStatusId { get; set; }
+        public DateTime TicketRaisedTimestamp { get; set; }
+        public int? AssignedTo { get; set; }
+        public int? ApproverId { get; set; }
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
