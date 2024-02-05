0fdca6ff2f205a258f663c092d60ff754edd19dd
Julia Johny Puthenpurayil <julia.puthenpurayil@experionglobal.local>
2024-02-05 12:19:45 +0530

    Added dev

---
+++ FacilitEase/Models/ApiModels/CommentRequestDto.cs
@@ -0,0 +1,6 @@
+﻿namespace FacilitEase.Models.ApiModels
+{
+    public class CommentRequestDto
+    {
+    }
+}
--- FacilitEase/Services/IL3AdminService.cs
+++ FacilitEase/Services/IL3AdminService.cs
@@ -6,23 +6,22 @@ namespace FacilitEase.Services
     public interface IL3AdminService
     {
         void AddTicket(TBL_TICKET ticket);
-
         void CloseTicket(int ticketId);
+        public void AcceptTicketCancellation(int ticketId);
+        public void DenyTicketCancellation(int ticketId);
 
         void ForwardTicket(int ticketId, int managerId);
-
         void ForwardTicketToDept(int ticketId, int deptId);
-
         public string GetCommentTextByTicketId(int ticketId);
-
         public void UpdateCommentTextByTicketId(int ticketId, string newText);
-
-        public IEnumerable<TicketJoin> GetEscalatedTicketsByAgent(int agentId);
-
+        public void AddComment(TBL_COMMENT comment);
+        public Task<bool> DeleteCommentAsync(int ticketId);
         IEnumerable<Join> GetTicketDetailByAgent(int ticketId);
-
         public AgentTicketResponse<TicketJoin> GetTicketsByAgent(int agentId, string sortField, string sortOrder, int pageIndex, int pageSize, string searchQuery);
-
         public AgentTicketResponse<TicketResolveJoin> GetResolvedTicketsByAgent(int agentId, string sortField, string sortOrder, int pageIndex, int pageSize, string searchQuery);
+        public AgentTicketResponse<TicketResolveJoin> GetOnHoldTicketsByAgent(int agentId, string sortField, string sortOrder, int pageIndex, int pageSize, string searchQuery);
+        public AgentTicketResponse<TicketResolveJoin> GetCancelRequestTicketsByAgent(int agentId, string sortField, string sortOrder, int pageIndex, int pageSize, string searchQuery);
+
+
     }
 }
