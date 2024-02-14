01c699a8c54344fedf3731465138dcfec0023233
abhijith.nair <abhikannan51@gmail.com>
2024-02-14 17:27:59 +0530

    added some comments

--- FacilitEase/Services/ITicketService.cs
+++ FacilitEase/Services/ITicketService.cs
@@ -7,16 +7,62 @@ namespace FacilitEase.Services
     {
         public ManagerTicketResponse<ManagerEmployeeTickets> GetTicketByManager(int managerId, string sortField, string sortOrder, int pageIndex, int pageSize, string searchQuery);
 
+        /// <summary>
+        /// Get - Retrieves a list of tickets raised by the emloyees that are currently live,  working under a specific manager and the total number of tickets
+        /// Includes pagination,searching and sorting functionality.
+        /// </summary>
+        /// <param name="managerId"></param>
+        /// <param name="sortField"></param>
+        /// <param name="sortOrder"></param>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="searchQuery"></param>
+        /// <returns>A response of paginated list of tickets of employees associated with a specific manager and the total tickets count</returns>
         public ManagerTicketResponse<ManagerEmployeeTickets> GetLiveTicketByManager(int managerId, string sortField, string sortOrder, int pageIndex, int pageSize, string searchQuery);
 
+        /// <summary>
+        /// Get - Retrieves all the data required when the manager accesses the detailed view of a specific employee ticket
+        /// </summary>
+        /// <param name="ticketId"></param>
+        /// <returns></returns>
         public ManagerEmployeeTicketDetailed ViewTicketDetails(int ticketId);
 
+        /// <summary>
+        /// Get - Retrieves a list of tickets raised by the emloyees that need approval from the manager and the total number of waiting tickets
+        /// Includes pagination,searching and sorting functionality.
+        /// </summary>
+        /// <param name="managerId"></param>
+        /// <param name="sortField"></param>
+        /// <param name="sortOrder"></param>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="searchQuery"></param>
+        /// <returns></returns>
         public ManagerTicketResponse<ManagerEmployeeTickets> GetApprovalTicket(int managerId, string sortField, string sortOrder, int pageIndex, int pageSize, string searchQuery);
 
+        /// <summary>
+        /// Post - Decides whether the ticket needs to be accepted or rejected
+        /// Updates the status of a ticket to inprogress or cancelled and changes controller to the agent or null.
+        /// </summary>
+        /// <param name="ticketId"></param>
+        /// <param name="statusId"></param>
+        /// <exception cref="InvalidOperationException"></exception>
         public void TicketDecision(int ticketId, int statusId);
 
+        /// <summary>
+        /// Post - Updates the priority id according to the priority specified by the Manager
+        /// </summary>
+        /// <param name="ticketId"></param>
+        /// <param name="newPriorityId"></param>
+        /// <exception cref="InvalidOperationException"></exception>
         public void ChangePriority(int ticketId, int newPriorityId);
 
+        /// <summary>
+        /// Post - Updates the controller id to the department head id if the manager things higher authority approval is necessary
+        /// </summary>
+        /// <param name="ticketId"></param>
+        /// <param name="managerId"></param>
+        /// <exception cref="InvalidOperationException"></exception>
         public void SendForApproval(int ticketId, int managerId);
 
         public DepartmentHeadManagerTicketDetails DHTicketDetails(int ticketId);
